96c23ae6f03a26ed45112c9c5fe496fbe67d50e6
vernal <vernal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-13 20:08:32 +0000

    deploy from build of 1.0.0-beta14
    git-svn-id: https://plugins.svn.wordpress.org/postmatic/trunk@1067326 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- postmatic.php
+++ postmatic.php
@@ -2,7 +2,7 @@
 /*
   Plugin Name: Postmatic - WordPress Subscriptions & Commenting by Email
   Description:
-  Version: 1.0.0-beta13
+  Version: 1.0.0-beta14
   License: GPL2+
   Author: Postmatic
   Author URI: http://gopostmatic.com/
